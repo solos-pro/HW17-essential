57ff5d928d0e6d7cc693406d39af1148ff4ae42c
solos-pro <sergey-solonichenko@rambler.ru>
2022-02-13 16:29:51 +0300

    All work well! A search is improved.

--- app.py
+++ app.py
@@ -54,17 +54,11 @@ genre_ns = api.namespace('genres')
 @movie_ns.route('/')
 class MoviesView(Resource):
     def get(self):
-        director_id = request.args.get('director_id')
-        genre_id = request.args.get('genre_id')
-        # res = Movie.query
-        # db.session.query(User.id, User.name, Group.name.label("grp_name")).join(Group).all()
         res = db.session.query(Movie.id, Movie.title, Genre.name).join(Genre).join(Director)
-        if director_id is not None:
-            res = res.filter(Movie.director_id == director_id)
-        if genre_id is not None:
-            res = res.filter(Movie.genre_id == genre_id)
-        if genre_id is not None and director_id is not None:
-            res = res.filter(Movie.genre_id == genre_id, Movie.director_id == director_id)
+        if request.args.get('director_id') is not None:
+            res = res.filter(Movie.director_id == request.args.get('director_id'))
+        if request.args.get('genre_id') is not None:
+            res = res.filter(Movie.genre_id == request.args.get('genre_id'))
         result = res.all()
 
         return movie_schema.dump(result, many=True), 200
@@ -207,3 +201,4 @@ class DirectorView(Resource):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
